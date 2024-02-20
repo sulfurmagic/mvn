9828bea96f586826003d7801a5138ed998a13b8b
sulfurmagic <johnaul81@gmail.com>
2024-02-20 13:31:33 +0300

    reformated code

--- src/main/java/ru/netology/homework/Main.java
+++ src/main/java/ru/netology/homework/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
         BonusService service = new BonusService();
 
-           int expected = 150;
-            long actual = service.calculate(5_000, true);
+        int expected = 150;
+        long actual = service.calculate(5_000, true);
         System.out.println(expected + " == " + actual);
 
 
